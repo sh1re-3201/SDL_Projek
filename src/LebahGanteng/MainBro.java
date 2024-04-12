2bd4966bec58af09e4fdfb17d0e29633ea5bc51c
sh1re-3201 <anugrahmapia@gmail.com>
2024-04-12 23:43:16 +0700

    Nambah import Javax

--- src/LebahGanteng/MainBro.java
+++ src/LebahGanteng/MainBro.java
@@ -2,5 +2,27 @@ package LebahGanteng;
 
 import javax.swing.*;
 public class MainBro {
+    public static void main(String[] args) {
+        //Objek dan Manggil Kelas
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Menu GUI
+    }
 }
