ed09563a3663ab84a4179207d5a0a686a7da8d51
Frathol <michaelmosses35746@gmail.com>
2024-04-13 01:27:14 +0700

    ok

--- src/LebahGanteng/MainBro.java
+++ src/LebahGanteng/MainBro.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 public class MainBro{
     public static void main(String[] args) {
         //Objek dan Manggil Kelas
-<<<<<<< Updated upstream
         JOptionPane optionPane = new JOptionPane("Selamat Datang!", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{}, null);
         JDialog dialog = optionPane.createDialog(null);
 
@@ -32,9 +31,11 @@ public class MainBro{
         lama.start();
         dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         dialog.setVisible(true);
+        
+        String input = JOptionPane.showInputDialog(null, "Masukkan jumlah mahasiswa yang ingin anda masukkan nilainya.");
+        
 
         while (!keluar) {
-            String input = JOptionPane.showInputDialog(null, "Masukkan jumlah mahasiswa yang ingin anda masukkan nilainya.");
             if (input == null) {// If statement ini berfungsi untuk keluar dari program jika user mengklik tombol X(silang) pada jendela
                 return;
             }
@@ -55,51 +56,54 @@ public class MainBro{
         }
 
         // Array of Objek
-
+        classGetSet[] isiData = new classGetSet[jumlahMhs];
         // Bagian kode untuk meminta identitas mahasiswa
         // laporan diganti sesuai dengan nama array yang dipilih
-        for (int i = 0; i < laporan.length;) {
+        for (int i = 0; i < isiData.length;) {
             String namaMhs = JOptionPane.showInputDialog(null, "Masukkan nama mahasiswa " + (i + 1));
+            
             if (namaMhs == null) {
                 return;
             }
 
-=======
-        
->>>>>>> Stashed changes
 
-            String nimMhs = JOptionPane.showInputDialog(null, "Masukkan nim mahasiswa " + (i + 1));
-            if (nimMhs == null) {
+            int nimMhs = Integer.parseInt(JOptionPane.showInputDialog(null, "Masukkan nim mahasiswa " + namaMhs));
+           
+            if (isiData[nimMhs] == null) {
                 return;
             }
 
+            int nilaiMhs = Integer.parseInt(JOptionPane.showInputDialog(null, "Masukkan nilai mahasiswa " + namaMhs));
+            isiData[i].setnilai(nilaiMhs);
+            if(isiData[nilaiMhs] == null){
+                return;
+            }
             /*
             if statement ini digunakan agar tidak ada data kosong yang bisa
             lewat dan dimasukkan ke array.
             dalam else terdapat increment yang bertujuan untuk memajukan
             for loop hanya jika seluruh data terisi.
              */
-            if (namaMhs.isEmpty() || nimMhs.isEmpty() || tglLahirMhs.isEmpty()) {
+            if (namaMhs.isEmpty() || isiData[nimMhs].isEmpty() || isiData[nilaiMhs].isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Tidak boleh ada data inputan yang kosong. Silakan coba lagi.");
             } else {
-
-                // Ini untuk ngeset data ke array of objek
-                laporan[i] = new Mahasiswa(namaMhs);
-                laporan[i].setNim(nimMhs);
+                isiData[i].setnama(namaMhs);
+                isiData[i].setnim(nimMhs);
+                isiData[i].setnilai(nilaiMhs);
                 i++;// Increment untuk for loop
             }
 
             boolean boolNilai = false;
             while (!boolNilai){
                 boolNilai = true;
-                for (int i = 0; i < laporan.length; i++) {
-                    String nilaiTotal = JOptionPane.showInputDialog(null, "Masukkan nilai Total dari : " + laporan[i].getNama());
+                for (int j = 0; i < isiData.length; i++) {
+                    String nilaiTotal = JOptionPane.showInputDialog(null, "Masukkan nilai Total dari : " + isiData[i].getnama());
                     if (nilaiTotal == null) {
                         return;
                     }
 
                     try {
-                        laporan[i].setUts1(Double.parseDouble(nilaiTotal));
+                        isiData[j].setnilai(Integer.parseInt(nilaiTotal));
 
                     } catch (NumberFormatException e) {
                         JOptionPane.showMessageDialog(null, """
